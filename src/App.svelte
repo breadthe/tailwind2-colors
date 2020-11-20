3638b65d6e3928aa1540d73e1001f86a89f62fac
breadthe <omigoshdev@protonmail.com>
2020-11-19 22:44:03 -0600

    Zoom swatch on hover

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,24 @@
   );
 
   const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+  function zoom(id, direction) {
+    const el = document.getElementById(id);
+
+    if (direction === "in") {
+      el.style.zIndex = 1;
+      el.style.transform = "scale(2)";
+      el.classList.add("shadow", "ring-4", `ring-${id}`, "ring-opacity-50");
+    }
+
+    if (direction === "out") {
+      el.style.zIndex = 0;
+      el.style.transform = "scale(1)";
+      el.classList.remove("shadow", "ring-4", `ring-${id}`, "ring-opacity-50");
+    }
+
+    return;
+  }
 </script>
 
 <style>
@@ -22,9 +40,12 @@
     {#each filteredColors as color}
       {#each shades as shade}
         <div
+          id={`${color}-${shade}`}
           class="flex items-center justify-center text-center {`bg-${color}-${shade}`}
           {shade > 400 ? `text-${color}-50` : `text-${color}-900`}"
-          style="font-size: 0.5rem;">
+          style="font-size: 0.5rem;"
+          on:mouseover={() => zoom(`${color}-${shade}`, 'in')}
+          on:mouseout={() => zoom(`${color}-${shade}`, 'out')}>
           {color} {shade}
         </div>
       {/each}
